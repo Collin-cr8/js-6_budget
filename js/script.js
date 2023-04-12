abfffc9cc728310b6df2e3d8694fc47b6edbfd35
Collin R <collin.codes.hard@gmail.com>
2023-04-12 18:38:25 -0500

    Created tripInfo function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 //Select paragraph and Button
 var tripInfoButton = document.querySelector(".trip-info");
 var dailyBudget = document.querySelector(".daily-budget");
+
+var tripInfo = function () {
+    var totalBudget = Number(prompt("What is your total budget?"));
+    var accomodation = Number(prompt("What are your accomodation costs?"));
+    var numDays = Number(prompt("How many days does your trip last?"));
+    calculateDailyBudget(totalBudget, accomodation, numDays);
+};
